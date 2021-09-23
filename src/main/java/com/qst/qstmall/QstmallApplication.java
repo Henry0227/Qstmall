b5c23f1c4dcfbc218810ec8d78432c5d95917e86
46749 <467495711@qq.com>
2021-09-23 10:59:13 +0800

    首次提交

---
+++ src/main/java/com/qst/qstmall/QstmallApplication.java
@@ -0,0 +1,13 @@
+package com.qst.qstmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QstmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QstmallApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/qst/qstmall/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.qst.qstmall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminController {
+
+    @RequestMapping("/test")
+    public ModelAndView test(){
+        ModelAndView view = new ModelAndView();
+        view.setViewName("/a");
+        view.addObject("msg","zhangsan");
+
+        return view;
+    }
+
+}
