bb049b7df358427563349e1255ee3f8fb31d1468
46749 <467495711@qq.com>
2021-10-12 09:13:13 +0800

    任务14：后台订单管理

---
+++ src/main/java/com/qst/qstmall/controller/QstMallOrderController.java
@@ -0,0 +1,134 @@
+package com.qst.qstmall.controller;
+
+import com.qst.qstmall.common.ServiceResultEnum;
+import com.qst.qstmall.controller.vo.QstMallOrderItemVO;
+import com.qst.qstmall.entity.QstMallOrder;
+import com.qst.qstmall.service.QstMallOrderService;
+import com.qst.qstmall.utils.PageQueryUtil;
+import com.qst.qstmall.utils.Result;
+import com.qst.qstmall.utils.ResultGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+
+@Controller
+@RequestMapping("/admin")
+public class QstMallOrderController {
+
+    @Resource
+    private QstMallOrderService qstMallOrderService;
+
+    @GetMapping("/orders")
+    public String ordersPage(HttpServletRequest request) {
+        request.setAttribute("path", "orders");
+        return "admin/qst_mall_order";
+    }
+
+    /**
+     * 列表
+     */
+    @RequestMapping(value = "/orders/list", method = RequestMethod.GET)
+    @ResponseBody
+    public Result list(@RequestParam Map<String, Object> params) {
+        if (StringUtils.isEmpty(params.get("page")) || StringUtils.isEmpty(params.get("limit"))) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        PageQueryUtil pageUtil = new PageQueryUtil(params);
+        return ResultGenerator.genSuccessResult(qstMallOrderService.getQstMallOrdersPage(pageUtil));
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping(value = "/orders/update", method = RequestMethod.POST)
+    @ResponseBody
+    public Result update(@RequestBody QstMallOrder qstMallOrder) {
+        if (Objects.isNull(qstMallOrder.getTotalPrice())
+                || Objects.isNull(qstMallOrder.getOrderId())
+                || qstMallOrder.getOrderId() < 1
+                || qstMallOrder.getTotalPrice() < 1
+                || StringUtils.isEmpty(qstMallOrder.getUserAddress())) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = qstMallOrderService.updateOrderInfo(qstMallOrder);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+    /**
+     * 详情
+     */
+    @GetMapping("/order-items/{id}")
+    @ResponseBody
+    public Result info(@PathVariable("id") Long id) {
+        List<QstMallOrderItemVO> orderItems = qstMallOrderService.getOrderItems(id);
+        if (!CollectionUtils.isEmpty(orderItems)) {
+            return ResultGenerator.genSuccessResult(orderItems);
+        }
+        return ResultGenerator.genFailResult(ServiceResultEnum.DATA_NOT_EXIST.getResult());
+    }
+
+    /**
+     * 配货
+     */
+    @RequestMapping(value = "/orders/checkDone", method = RequestMethod.POST)
+    @ResponseBody
+    public Result checkDone(@RequestBody Long[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = qstMallOrderService.checkDone(ids);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+    /**
+     * 出库
+     */
+    @RequestMapping(value = "/orders/checkOut", method = RequestMethod.POST)
+    @ResponseBody
+    public Result checkOut(@RequestBody Long[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = qstMallOrderService.checkOut(ids);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+    /**
+     * 关闭订单
+     */
+    @RequestMapping(value = "/orders/close", method = RequestMethod.POST)
+    @ResponseBody
+    public Result closeOrder(@RequestBody Long[] ids) {
+        if (ids.length < 1) {
+            return ResultGenerator.genFailResult("参数异常！");
+        }
+        String result = qstMallOrderService.closeOrder(ids);
+        if (ServiceResultEnum.SUCCESS.getResult().equals(result)) {
+            return ResultGenerator.genSuccessResult();
+        } else {
+            return ResultGenerator.genFailResult(result);
+        }
+    }
+
+
+}
